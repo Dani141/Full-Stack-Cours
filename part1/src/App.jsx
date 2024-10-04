ae6c3031dca736cbd8517aad78a30eec596d71f3
Dani141 <danielam141200@gmail.com>
2024-10-04 16:23:21 -0400

    Ex 1.1.-1.2.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,49 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+const Part = ({ name, exercises }) => {
+  return (
+    <p>{name} {exercises}</p>
+  )
+}
 
+const Content = ({ part1, part2, part3, ex1, ex2, ex3 }) => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Part name={part1} exercises={ex1} />
+      <Part name={part2} exercises={ex2} />
+      <Part name={part3} exercises={ex3} />
     </>
+  )
+}
+
+const Total = ({ ex1, ex2, ex3 }) => {
+  return (
+    <p>Number of exercises {ex1 + ex2 + ex3}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+    </div>
   )
 }
 
